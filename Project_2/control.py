e3d5f84c57d52dfc1741a6cd5a8d67dda6498305
frederik <frederik.wagner@tugraz.student.at>
2025-12-03 20:07:32 +0100

    Assignment2 submission

--- Project_2/control.py
+++ Project_2/control.py
@@ -25,7 +25,11 @@ def print_stats(ctl):
     constraints_ternary = ctl.statistics["problem"]["generator"]["constraints_ternary"]
     constraints_other = ctl.statistics["problem"]["generator"]["constraints"]
     constraints = int(constraints_binary + constraints_ternary + constraints_other)
-    print("Constraints  : " + str(constraints) + "   (Binary:  " + "{:.1f}".format(100 * constraints_binary / constraints) + "% Ternary:  " + "{:.1f}".format(100 * constraints_ternary / constraints) + "% Other:   " + "{:.1f}".format(100 * constraints_other / constraints) + "%)")
+    print("Constraints  : " + str(constraints), end= " ")
+    if constraints > 0:
+        print("   (Binary:  " + "{:.1f}".format(100 * constraints_binary / constraints) + "% Ternary:  " + "{:.1f}".format(100 * constraints_ternary / constraints) + "% Other:   " + "{:.1f}".format(100 * constraints_other / constraints) + "%)")
+    else:
+        print("   (Binary:  0%  Ternary: 0%  Other: 0%)")
 
 # print model(s) together with a running number and read off next state + events
 def on_model(m):
@@ -70,69 +74,79 @@ def on_model(m):
                 args.append(Number(n+1))
                 event.append(Function(atom.name[5:], args))
         else: print(atom, end=" ")
-    print("\nOptimization: " + str(m.cost[0]))
+    if m.cost:
+        print("\nOptimization: " + str(m.cost[0]))
+    else:
+        print("\nOptimization: N/A")
 
 step = -1
 time = 0
 todo = True
+
+ctl = clingo.Control(arguments = ["--opt-strategy", "usc", "--warn", "none"])
+for arg in sys.argv[1:]: 
+    ctl.load(arg)
+ctl.load("elevator.lp")
+ctl.load("next.lp")
+ctl.ground([("instance", []), ("events", [])])
+
 state = []
 event = []
 
+for atom in ctl.symbolic_atoms.by_signature("init", 2):
+    args = atom.symbol.arguments
+    args.append(Number(0))
+    state.append(Function("at", args))
+for atom in ctl.symbolic_atoms.by_signature("call", 2):
+    args = atom.symbol.arguments
+    args.append(Number(0))
+    state.append(Function("todo_call", args))
+    args.append(Number(0))
+    event.append(Function("call", args))
+for atom in ctl.symbolic_atoms.by_signature("deliver", 2):
+    args = atom.symbol.arguments
+    args.append(Number(0))
+    state.append(Function("todo_deliver", args))
+for atom in ctl.symbolic_atoms.by_signature("priority", 2):
+    args = atom.symbol.arguments
+    args.append(Number(0))
+    state.append(Function("priority", args))
+for atom in ctl.symbolic_atoms.by_signature("call", 3):
+    args = atom.symbol.arguments
+    args.append(Number(0))
+    event.append(Function("call", args))
+for atom in ctl.symbolic_atoms.by_signature("call_deliver", 4):
+    args = atom.symbol.arguments
+    args.append(Number(0))
+    event.append(Function("call_deliver", args))
+
+print("INITIAL STATE:", [str(a) for a in state])
+print("INITIAL EVENTS:", [str(a) for a in event])
+
+active_externals = []
 while todo:
     step = step+1
     todo = False
     answer = 1
 
-### DO SOMETHING LIKE THIS BEFORE THE WHILE LOOP TO PERFORM MULTI-SHOT SOLVING
-    ctl = clingo.Control(arguments = ["--opt-strategy", "usc", "--warn", "none"])
-    for arg in sys.argv[1:]: ctl.load(arg)
-    ctl.load("next.lp")
+    for ext in active_externals:
+        ctl.assign_external(ext, False)
 
-    ctl.ground([("instance", []), ("events", [])])
-    if step == 0:
-        for atom in ctl.symbolic_atoms.by_signature("init", 2):
-            args = atom.symbol.arguments
-            args.append(Number(0))
-            state.append(Function("at", args))
-        for atom in ctl.symbolic_atoms.by_signature("call", 2):
-            args = atom.symbol.arguments
-            args.append(Number(0))
-            state.append(Function("todo_call", args))
-            args.append(Number(0))
-            event.append(Function("call", args))
-        for atom in ctl.symbolic_atoms.by_signature("deliver", 2):
-            args = atom.symbol.arguments
-            args.append(Number(0))
-            state.append(Function("todo_deliver", args))
-        for atom in ctl.symbolic_atoms.by_signature("priority", 2):
-            args = atom.symbol.arguments
-            args.append(Number(0))
-            state.append(Function("priority", args))
-        for atom in ctl.symbolic_atoms.by_signature("call", 3):
-            args = atom.symbol.arguments
-            args.append(Number(0))
-            event.append(Function("call", args))
-        for atom in ctl.symbolic_atoms.by_signature("call_deliver", 4):
-            args = atom.symbol.arguments
-            args.append(Number(0))
-            event.append(Function("call_deliver", args))
+    active_externals = []
 
-        print("INITIAL STATE:", end="  ")
-        print([str(a) for a in state])
-        print("INITIAL EVENTS:", end=" ")
-        print([str(a) for a in event])
-### THE WHOLE ABOVE PART SHOULD NOT BE IN THE WHILE LOOP FOR MULTI-SHOT SOLVING
+    for atom in state:
+        sym = clingo.Function(atom.name, atom.arguments)
+        ctl.assign_external(sym, True)
+        active_externals.append(sym)
 
-    facts_name = "fact_" + str(step) # UTILIZE EXTERNALS FOR MULTI-SHOT SOLVING
-    events_name = "event_" + str(step) # the event program can be used as it is
-
-    if state: ctl.add(facts_name, [], ".".join([str(a) for a in state]) + ".")
+    events_name = "event_" + str(step)
     if event: ctl.add(events_name, [], ".".join([str(a) for a in event]) + ".")
+
+    ctl.ground([("next", [Number(step)]), (events_name, [])])
 
     print("===========================")
     print("CALL " + str(step) + " AT TIME " + str(time))
 
-    ctl.ground([("base", []), ("next", [Number(step)]), (events_name, []), (facts_name, [])])
     ctl.solve(on_model = on_model)
 
     print("NEXT STATE:", end="  ")
